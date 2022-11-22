92f75c181ba1d8f57987b261a49ed5a846b6c323
truzicic <truzicic@gmail.com>
2022-11-22 19:08:45 +0100

    unesene vrijednosti za zupana i zupaniju

--- vjezba1.sql
+++ vjezba1.sql
@@ -35,3 +35,15 @@ alter table zupanija add foreign key (zupan) references zupan(sifra);
 alter table opcina add foreign key (zupanija) references zupanija(sifra);
 
 alter table mjesto add foreign key (opcina) references opcina(sifra);
+
+insert into zupan(ime,prezime) values ('Pero','Peric');
+insert into zupan(ime,prezime) values ('Marko','Maric');
+insert into zupan(ime,prezime) values ('Ivo','Ivic');
+insert into zupan(ime,prezime) values ('Spuzva','Bob');
+insert into zupan(ime,prezime) values ('Spiro','Spula');
+
+insert into zupanija(naziv,zupan,povrsina,brstanovnika) values ('Oslaka',1,23456.45,234000);
+insert into zupanija(naziv,zupan,povrsina,brstanovnika) values ('Jamajica',2,23987.34,187000);
+insert into zupanija(naziv,zupan,povrsina,brstanovnika) values ('Zelenland',3,134987.00,34000);
+insert into zupanija(naziv,zupan,povrsina,brstanovnika) values ('Babua Nova Korea',4,1287435.45,1234000);
+insert into zupanija(naziv,zupan,povrsina,brstanovnika) values ('Pogbala Crnovlasi',5,856324.23,987000);
