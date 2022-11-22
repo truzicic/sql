66ab5e708e37b00c9e7d35476fa31a7ff7382744
truzicic <truzicic@gmail.com>
2022-11-22 20:14:34 +0100

    Dodani nazivi mjesta, opcina i zupanija

--- vjezba1.sql
+++ vjezba1.sql
@@ -13,10 +13,8 @@ create table zupanija(
 create table opcina(
     sifra int not null primary key auto_increment,
     zupanija int,
-    naziv varchar(50),
-    povrsina decimal(50,2),
-    brstanovnika int
-);
+    naziv varchar(50)
+   );
 
 create table mjesto(
     sifra int not null primary key auto_increment,
@@ -47,3 +45,121 @@ insert into zupanija(naziv,zupan,povrsina,brstanovnika) values ('Jamajica',2,239
 insert into zupanija(naziv,zupan,povrsina,brstanovnika) values ('Zelenland',3,134987.00,34000);
 insert into zupanija(naziv,zupan,povrsina,brstanovnika) values ('Babua Nova Korea',4,1287435.45,1234000);
 insert into zupanija(naziv,zupan,povrsina,brstanovnika) values ('Pogbala Crnovlasi',5,856324.23,987000);
+
+insert into opcina(zupanija,naziv) values (1,'Kotjo');
+insert into opcina(zupanija,naziv) values (1,'MAGAsima');
+insert into opcina(zupanija,naziv) values (1,'Slavabaki');
+insert into opcina(zupanija,naziv) values (1,'Komsuza');
+
+insert into opcina(zupanija,naziv) values (2,'Abajtju');
+insert into opcina(zupanija,naziv) values (2,'Heklerimog');
+insert into opcina(zupanija,naziv) values (2,'Blenetan');
+insert into opcina(zupanija,naziv) values (2,'Oskokos');
+
+insert into opcina(zupanija,naziv) values (3,'Rotled');
+insert into opcina(zupanija,naziv) values (3,'Gelbmed');
+insert into opcina(zupanija,naziv) values (3,'Gerufed');
+insert into opcina(zupanija,naziv) values (3,'Blauei');
+
+insert into opcina(zupanija,naziv) values (4,'Orikori');
+insert into opcina(zupanija,naziv) values (4,'Alibali');
+insert into opcina(zupanija,naziv) values (4,'Vishibasi');
+insert into opcina(zupanija,naziv) values (4,'Rusipusi');
+
+insert into opcina(zupanija,naziv) values (5,'Baleland');
+insert into opcina(zupanija,naziv) values (5,'Ronaldam');
+insert into opcina(zupanija,naziv) values (5,'Lukalug');
+insert into opcina(zupanija,naziv) values (5,'Pelegrino');
+
+insert into mjesto (opcina,naziv) values (1,'Stormfury Wharf');
+insert into mjesto (opcina,naziv) values (1,'Cudfail Port');
+insert into mjesto (opcina,naziv) values (1,'Driftwood Piers');
+
+insert into mjesto (opcina,naziv) values (2,'Misty River Grange');
+insert into mjesto (opcina,naziv) values (2,'Broken Arrow Gardens');
+insert into mjesto (opcina,naziv) values (2,'Grand Mountain Ranch');
+
+insert into mjesto (opcina,naziv) values (3,'Nuniollar');
+insert into mjesto (opcina,naziv) values (3,'Breennosam');
+insert into mjesto (opcina,naziv) values (3,'Reellion');
+
+insert into mjesto (opcina,naziv) values (4,'Jyndunyrth');
+insert into mjesto (opcina,naziv) values (4,'Bulbydyg');
+insert into mjesto (opcina,naziv) values (4,'Taras');
+
+insert into mjesto (opcina,naziv) values (5,'Persica');
+insert into mjesto (opcina,naziv) values (5,'Clivia');
+insert into mjesto (opcina,naziv) values (5,'Gallia');
+
+insert into mjesto (opcina,naziv) values (5,'Locuststump');
+insert into mjesto (opcina,naziv) values (5,'Larchbeard');
+insert into mjesto (opcina,naziv) values (5,'Cedarblade');
+
+insert into mjesto (opcina,naziv) values (6,'Devils Playground Tombs');
+insert into mjesto (opcina,naziv) values (6,'White Beach Cemetery');
+insert into mjesto (opcina,naziv) values (6,'Shadow Necropolis');
+
+insert into mjesto (opcina,naziv) values (7,'Alzekh');
+insert into mjesto (opcina,naziv) values (7,'Elixi');
+insert into mjesto (opcina,naziv) values (7,'Dhirnimvra');
+
+insert into mjesto (opcina,naziv) values (8,'Forebane Cave');
+insert into mjesto (opcina,naziv) values (8,'The Dusk Covert');
+insert into mjesto (opcina,naziv) values (8,'The Talon Base');
+
+insert into mjesto (opcina,naziv) values (9,'Camp Company');
+insert into mjesto (opcina,naziv) values (9,'Camp Woodpecker');
+insert into mjesto (opcina,naziv) values (9,'Camp Alpine');
+
+insert into mjesto (opcina,naziv) values (10,'Gravelberry Path');
+insert into mjesto (opcina,naziv) values (10,'Wickpon Notch');
+insert into mjesto (opcina,naziv) values (10,'Cornbalt Pass');
+
+insert into mjesto (opcina,naziv) values (11,'Westview Side');
+insert into mjesto (opcina,naziv) values (11,'Fountainvale Place');
+insert into mjesto (opcina,naziv) values (11,'McLachlan Side');
+
+insert into mjesto (opcina,naziv) values (12,'Dreampass Site');
+insert into mjesto (opcina,naziv) values (12,'Gloom Harbor');
+insert into mjesto (opcina,naziv) values (12,'Writhing Frontier');
+
+insert into mjesto (opcina,naziv) values (13,'The Hot Reef');
+insert into mjesto (opcina,naziv) values (13,'Coatiburg Key');
+insert into mjesto (opcina,naziv) values (13,'Rimpids Haven');
+
+insert into mjesto (opcina,naziv) values (14,'Nonine');
+insert into mjesto (opcina,naziv) values (14,'Graudriara');
+insert into mjesto (opcina,naziv) values (14,'Esopiams');
+
+insert into mjesto (opcina,naziv) values (15,'Veneration Square');
+insert into mjesto (opcina,naziv) values (15,'Honor Plaza');
+insert into mjesto (opcina,naziv) values (15,'Oracle Square');
+
+insert into mjesto (opcina,naziv) values (16,'Quarters of the Scarlet Orc');
+insert into mjesto (opcina,naziv) values (16,'Cells of the Ancient Phoenix');
+insert into mjesto (opcina,naziv) values (16,'The Serene Delves');
+
+insert into mjesto (opcina,naziv) values (17,'Leicaster Lagoon');
+insert into mjesto (opcina,naziv) values (17,'New Cove');
+insert into mjesto (opcina,naziv) values (17,'Bosware Depths');
+
+insert into mjesto (opcina,naziv) values (18,'The Matron');
+insert into mjesto (opcina,naziv) values (18,'Battlestar Fortress');
+insert into mjesto (opcina,naziv) values (18,'The Lament');
+
+insert into mjesto (opcina,naziv) values (19,'Setting Sun Gardens');
+insert into mjesto (opcina,naziv) values (19,'Tumbleweed Grange');
+insert into mjesto (opcina,naziv) values (19,'Whisperwind Farm');
+
+insert into mjesto (opcina,naziv) values (20,'Otterstall Hills');
+insert into mjesto (opcina,naziv) values (20,'The Ice-crowned Hill');
+insert into mjesto (opcina,naziv) values (20,'The Gray Heights');
+
+
+
+
+
+
+
+
+
