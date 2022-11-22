7f9fb6c351a17d5bca87b28dd05df6117d39fac1
truzicic <truzicic@gmail.com>
2022-11-22 18:45:57 +0100

    upisane tablice u bazu

---
+++ vjezba1.sql
@@ -0,0 +1,31 @@
+drop database if exists vjezba1;
+create database vjezba1;
+use vjezba1;
+
+create table zupanija(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50),
+    zupan int,
+    povrsina decimal(50,2),
+    brstanovnika int
+);
+
+create table opcina(
+    sifra int not null primary key auto_increment,
+    zupanija int,
+    naziv varchar(50),
+    povrsina decimal(50,2),
+    brstanovnika int
+);
+
+create table mjesto(
+    sifra int not null primary key auto_increment,
+    opcina int,
+    naziv varchar(50)
+);
+
+create table zupan(
+    sifra int not null primary key auto_increment,
+    ime varchar(50),
+    prezime varchar(50)
+);
